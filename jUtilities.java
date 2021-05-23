44d543e0aa521d25130b6dcf216fd71f9463f34a
ope-nz <ope.nz.geeks@gmail.com>
2021-05-24 11:47:06 +1200

    Update

--- jUtilities.java
+++ jUtilities.java
@@ -31,6 +31,7 @@ import java.io.InputStreamReader;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.io.BufferedInputStream;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.BufferedOutputStream;
 
@@ -713,5 +714,40 @@ public class jUtilities {
 			&& string.length() > 0
             && ("null".equals(string)			
             || (string.startsWith("[") && string.endsWith("]")) || (string.startsWith("{") && string.endsWith("}")));
-    }
+	}
+	
+	public static boolean mayBeBinary(String FilePath)
+	{
+		try {
+			FileInputStream in = new FileInputStream(FilePath);
+			int size = in.available();
+			if(size > 1024) size = 1024;
+			byte[] data = new byte[size];
+			in.read(data);
+			in.close();
+		
+			int ascii = 0;
+			int other = 0;
+		
+			for(int i = 0; i < data.length; i++) {
+				byte b = data[i];
+				if( b < 0x09 ) return true;
+		
+				if( b == 0x09 || b == 0x0A || b == 0x0C || b == 0x0D ) ascii++;
+				else if( b >= 0x20  &&  b <= 0x7E ) ascii++;
+				else other++;
+			}
+		
+			if( other == 0 ) return false;
+		
+			return 100 * other / (ascii + other) > 95;
+
+		}
+		catch(Exception ex)
+		{
+			ex.printStackTrace();
+		}
+
+		return false;
+	}
 }
